b9be29aca59498846f2dafbcd66c177f4322afdb
beatrizeg <beatriz.eg92@gmail.com>
2023-01-15 20:35:22 +0100

    IncludingCupCoffee

--- components/Timer.js
+++ components/Timer.js
@@ -6,7 +6,7 @@ export default function Timer(props) {
     
     return (
         <div id="timer-element">
-            <h2 id="timer-label">{props.session.charAt(0).toUpperCase() + props.session.slice(1)}</h2>
+            <h2 id="timer-label">{props.session == "session" ? props.session.charAt(0).toUpperCase() + props.session.slice(1) : <i className="fa-solid fa-mug-hot"></i>}</h2>
             <div id="time-left"><audio id="beep" src="https://www.pacdv.com/sounds/interface_sound_effects/sound10.mp3" type="audio/mp3"></audio>
             {min.toString().length === 1 ? "0" + min : min}:{sec.toString().length === 1 ? "0" + sec : sec}</div>
         </div>
